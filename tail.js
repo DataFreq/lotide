ef24c0d9b9dfabebcf80b8ea365a02ea01d543e3
vagrant <rpg7ofdeath@gmail.com>
2021-08-17 00:42:09 +0000

    remove head without changing org arr

---
+++ tail.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ [PASS] Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 [ERR] Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = (arr) => {
+  return arr.slice(1);
+};
