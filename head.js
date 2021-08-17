a98a3b10da4915bee970a3adc909ac98628c2953
vagrant <rpg7ofdeath@gmail.com>
2021-08-17 00:03:26 +0000

    assertion with array head function

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ [PASS] Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 [ERR] Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = (actual, expected) => {
+  return actual.shift()
+}
+
+// TEST
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
