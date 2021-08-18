00175d0c10d4560b91616468523c456893eb1421
vagrant <rpg7ofdeath@gmail.com>
2021-08-18 02:36:07 +0000

    init commit of without.js

---
+++ without.js
@@ -0,0 +1,30 @@
+const eqArrays = (arr, arr1) => {
+  if (arr.length !== arr1.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr1[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrEqual = (arr, arr1) => {
+  if (eqArrays(arr, arr1)) {
+    console.log(`✅ [PASS] Assertion Passed: ${arr} === ${arr1}`);
+  } else {
+    console.log(`🛑 [ERR] Assertion Failed: ${arr} !== ${arr1}`);
+  }
+};
+
+const without = (src, filter) => {
+  let filtered = [];
+  for (const ele of src) {
+    if (!filter.includes(ele)) {
+      filtered.push(ele);
+    }
+  }
+  return filtered;
+};
