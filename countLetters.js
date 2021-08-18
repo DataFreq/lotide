0df195250249ad08c5ebcd275bac6de44bd047d0
vagrant <rpg7ofdeath@gmail.com>
2021-08-18 21:35:15 +0000

    init commit of countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -8,5 +8,11 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = (input) => {
   const output = {};
-  
-}
+
+  for (const ltr of input) {
+    if (ltr !== ' ') {
+      output[ltr] ? output[ltr]++ : output[ltr] = 1;
+    }
+  }
+  return output;
+};
