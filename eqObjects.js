a5e8c1c23aa462092ff23d14d7c5762d6e7328fe
vagrant <rpg7ofdeath@gmail.com>
2021-08-19 02:30:34 +0000

    better implementation of eqArrays

--- eqObjects.js
+++ eqObjects.js
@@ -23,7 +23,7 @@ const eqObjects = (obj1, obj2) => {
   if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
   for (const key of Object.keys(obj1)) {
     if (Object.keys(obj2).includes(key)) {
-      if (Array.isArray(obj1[key]) || Array.isArray(obj2[key])) return eqArrays(obj1[key], obj2[key]);
+      if (Array.isArray(obj1[key])) return eqArrays(obj1[key], obj2[key]);
     }
   }
   return true;
