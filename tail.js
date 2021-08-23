ef3bd9a327845fb5546e3137848ed8ccba4be0d6
vagrant <rpg7ofdeath@gmail.com>
2021-08-23 20:03:47 +0000

    refactored code and moved test cases to new file

--- tail.js
+++ tail.js
@@ -1,11 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ [PASS] Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 [ERR] Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = (arr) => {
   return arr.slice(1);
 };
+
+module.exports = tail
